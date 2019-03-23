433dab21a81b23ce09a6f196f607edf597aa02f2
ZloyLeva <zloyleva@gmail.com>
2019-03-23 11:15:41 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component,Fragment } from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+export default () => {
+  return (
+      <Fragment>
+          <div className="container">
+              <div className="row">
+                  <div className="col my-5">
+                      <h1>Task manager</h1>
+                  </div>
+              </div>
+          </div>
 
-export default App;
+          <div className="container">
+              <div className="row">
+                  <div className="col-12  my-5 Add__New__Task">
+                      <h2 className="h3">
+                          Add new task
+                      </h2>
+                      <form className="">
+                          <div className="form-group row">
+                              <label htmlFor="taskName" className="col-sm-2 col-form-label">Task</label>
+                              <div className="col-sm-10">
+                                  <input type="text" className="form-control" id="taskName" placeholder="Enter task" />
+                              </div>
+                          </div>
+                          <button type="submit" className="btn btn-primary">Add task</button>
+                      </form>
+                  </div>
+                  <div className="col-12 Task__List">
+                      <h2 className="h3">
+                          Tasks list
+                      </h2>
+                      <div className="card my-2 Task__Item">
+                          <div className="card-body">
+                              <h5 className="card-title">Card title</h5>
+                          </div>
+                      </div>
+                      <div className="card my-2 Task__Item">
+                          <div className="card-body">
+                              <h5 className="card-title">Card title</h5>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </Fragment>
+  );
+};
